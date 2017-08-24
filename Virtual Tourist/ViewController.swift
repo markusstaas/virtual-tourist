eacc5bb0a964b93e4ee7e017769a8b386d6de91f
Markus Staas1 <markus.staas@lel-vn-l-m-0012.local>
2017-08-24 11:31:06 +0700

    Initial Commit

---
+++ Virtual Tourist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Virtual Tourist
+//
+//  Created by Markus Staas (Lazada eLogistics Group) on 8/24/17.
+//  Copyright © 2017 Markus Staas (Lazada eLogistics Group). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
